29de8221e5287b4a64a8d41a7d388acef93e8801
Vadim <gubaydullin03@internet.ru>
2025-04-23 16:17:23 +0300

    test1.py

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello")
-print("Hello python")
-=======
-print("Hello Python! test.py")
-print("New changes git cherry-pick 2")
->>>>>>> da0b858 (New changes test.py 2)
-=======
+print("print 1")
 print("print 2")
->>>>>>> 8404e29 (test2.py)
